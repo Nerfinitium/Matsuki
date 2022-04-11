6f98e760d9893b083af79f6d74f6888e5bc203c0
Xaiai <85024476+Nerfinitium@users.noreply.github.com>
2022-04-12 00:31:25 +0300

    Update inr.ts

--- commands/inr.ts
+++ commands/inr.ts
@@ -24,19 +24,26 @@ export default {
       
       Http.onreadystatechange = () => {
         console.log(Http.responseText)
-        //send embed
-        const exampleEmbed = new MessageEmbed()
-        
-       .setColor('ORANGE')
-        .setTitle('Monero')
-        .setDescription(` ${Http.responseText}`)
-        .setTimestamp()
-        .setFooter('XMR', 'https://s2.coinmarketcap.com/static/img/coins/200x200/328.png');
-        message.channel.send({ embeds: [exampleEmbed] });
-
-
       }
+//get changepercent24hr from the response and convert it to a 2 decimal place number
+      Http.onreadystatechange = () => {
+        if (Http.readyState == 4 && Http.status == 200) {
+          var response = JSON.parse(Http.responseText);
+          var changepercent24hr = response.data.changePercent24Hr;
+          var changepercent24hr2decimal = changepercent24hr.toFixed(2); // an error is happening here on .tofixed(2)
+          console.log(changepercent24hr2decimal)
 
-
+// sending embed 
+      const exampleEmbed = new MessageEmbed()
+      .setColor('ORANGE')
+       .setTitle('Monero')
+       //ChangePercentage24hr
+       .setDescription(`the chnage percentage is ${changepercent24hr}%`)
+       .setTimestamp()
+       .setFooter('XMR', 'https://s2.coinmarketcap.com/static/img/coins/200x200/328.png');
+       message.channel.send({ embeds: [exampleEmbed] });
      }
+     
+}
+}
 }
